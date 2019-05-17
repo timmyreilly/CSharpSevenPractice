178e5078106b0c429aac821603fe67ce59016b0e
timmyreilly <timreilly@live.com>
2019-05-16 18:14:09 -0700

    some properties in place

--- ClassesWelcome/Person.cs
+++ ClassesWelcome/Person.cs
@@ -8,10 +8,61 @@ namespace ClassesWelcome
         // protected - accessible to the class and derived 'sub-classes'
         // internal - can only be used by classes in the same assembly. 
 
-        public string FirstName; 
-        public string LastName; 
-        public int Age; 
+        public string FirstName;
+        public string LastName;
+        public int Age;
 
-        private decimal salary; 
+        private decimal salary;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary=45000)
+        {
+            FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age; // we can omit this if we want. 
+            this.salary = salary;
+        }
+
+        // how about some properties. 
+    }
+
+    public class PersonWithProperties 
+    {
+        private readonly string _firstName; 
+        private readonly string _lastName; // backing fields can only be set with constructor
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName; 
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName; 
+            }
+        }
+
+        public int Age{ get; set; }
+
+        private decimal _salary; 
+
+        public PersonWithProperties(string firstName, string lastName, int age, decimal salary = 45000)
+        {
+            _firstName = firstName; 
+            _lastName = lastName; 
+            Age = age; 
+
+            // default value
+            _salary = salary; 
+        }
     }
 }
--- ClassesWelcome/Program.cs
+++ ClassesWelcome/Program.cs
@@ -11,6 +11,17 @@ namespace ClassesWelcome
             person.LastName = "Smith";
             person.Age = 55; 
             Console.WriteLine("{0} {1} is aged: {2}", person.FirstName, person.LastName, person.Age); 
+
+            Person PersonTwo = new Person("Tim", "Reilly", 43); 
+            Console.WriteLine("{0} {1} is aged: {2}", PersonTwo.FirstName, PersonTwo.LastName, PersonTwo.Age); 
+
+
+            PersonWithProperties personThree = new PersonWithProperties("Bob", "Sacamano", 55); 
+            personThree.Age = 18; 
+            Console.WriteLine("{0} {1} is aged: {2}", personThree.FirstName, personThree.LastName, personThree.Age); 
+
+            Console.ReadKey(); 
+
         }
     }
 }
