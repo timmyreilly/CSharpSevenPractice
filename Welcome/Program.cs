281a1234f56e34a89f86635cba40c97518758885
timmyreilly <timreilly@live.com>
2019-05-16 17:32:38 -0700

    pretty neat

--- Welcome/Program.cs
+++ Welcome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace CSharpSeven
 {
@@ -6,10 +7,42 @@ namespace CSharpSeven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine(); 
-            Console.WriteLine("Hello, " + name); 
-            Console.ReadKey(); 
+            int myInt = 42;
+            uint myUnsigned = 10000;
+            char[] charArray = new char[] { 'a', 'b', 'c' };
+            Console.WriteLine(charArray.Length);
+            char[] charArrayTw = new char[4];
+            Console.WriteLine(charArrayTw.Length);
+            Console.WriteLine("myInt = {0}", myInt);
+
+            // string are immutable 
+            // don't do this: string result = "";
+            // do this: 
+            StringBuilder builder = new StringBuilder();
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Type a word: ");
+                string input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                else
+                {
+                    builder.Append(input + " ");
+                }
+            }
+
+            // you should use string builder. Adds them to the back of the list of strings. 
+
+
+
+            Console.WriteLine("Result: {0}", builder.ToString());
+            Console.ReadKey();
+
         }
     }
 }
