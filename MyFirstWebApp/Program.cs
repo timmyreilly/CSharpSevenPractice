bcc00c80e46c809f07d498bfcb7564d50801270a
timmyreilly <timreilly@live.com>
2019-05-16 17:17:32 -0700

    added a different web host builder

--- MyFirstWebApp/Program.cs
+++ MyFirstWebApp/Program.cs
@@ -12,9 +12,21 @@ namespace MyFirstWebApp
 {
     public class Program
     {
+        // public static void Main(string[] args)
+        // {
+        //     CreateWebHostBuilder(args).Build().Run();
+        // }
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run(); 
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
