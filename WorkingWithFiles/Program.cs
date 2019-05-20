addb1df2153207c527db1dc85ff5cc77f3b6c320
timmyreilly <timreilly@live.com>
2019-05-20 16:30:35 -0700

    error handling is helpful for files

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -7,33 +7,68 @@ namespace WorkingWithFiles
     {
         static void Main(string[] args)
         {
-            readFile(); 
-            writeFile(); 
-            UsingReadFile(); 
-            Console.ReadKey(); 
+            readFile();
+            writeFile();
+            UsingReadFile();
+            UsingReadFileWhoops(); 
+            Console.ReadKey();
         }
 
-        private static void readFile() 
+        private static void readFile()
         {
             StreamReader reader = new StreamReader("test.txt");
-            string context = reader.ReadToEnd(); 
-            Console.WriteLine(context); 
-            reader.Close();  
+            string context = reader.ReadToEnd();
+            Console.WriteLine(context);
+            reader.Close();
         }
 
-        private static void writeFile() 
+        private static void writeFile()
         {
-            StreamWriter writer = new StreamWriter("test-text.txt", false); 
-            writer.WriteLine("VS COde is great"); 
-            writer.Close(); 
+            StreamWriter writer = new StreamWriter("test-text.txt", false);
+            writer.WriteLine("VS COde is great");
+            writer.Close();
         }
 
-        private static void UsingReadFile() 
+        private static void UsingReadFile()
         {
-            using(StreamReader reader = new StreamReader("test-text.txt"))
+            using (StreamReader reader = new StreamReader("test-text.txt"))
             {
-                string contents = reader.ReadToEnd(); 
-                Console.WriteLine(contents); 
+                string contents = reader.ReadToEnd();
+                Console.WriteLine(contents);
+            }
+        }
+
+        private static void UsingReadFileWhoops()
+        {
+            try
+            {
+
+                using (StreamReader reader = new StreamReader("test-not-there.txt"))
+                {
+                    string contents = reader.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            }
+            catch(FileNotFoundException e) 
+            {
+                Console.WriteLine("File not found"); 
+            }
+            catch(FileLoadException e) 
+            {
+                Console.WriteLine("File load failed"); 
+            }
+            catch(Exception) 
+            {
+                Console.WriteLine("File operation failed"); 
+            }
+        }
+
+        private static async void UsingReadFileAsync()
+        {
+            using (StreamReader reader = new StreamReader("test-text.txt"))
+            {
+                string contents = await reader.ReadToEndAsync();
+                Console.WriteLine(contents);
             }
         }
     }
