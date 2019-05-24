f3f6e3345b6013b4de2a8813770be56fe582d0c3
timmyreilly <timreilly@live.com>
2019-05-24 08:56:39 -0700

    some good stuff for delegates

---
+++ DelegatesLambdaAndMore/Machine.cs
@@ -0,0 +1,50 @@
+namespace DelegatesLambdaAndMore
+{
+
+    public delegate void TempChanged(double prev, double next);
+    public class Machine
+    {
+        private double _currentTemp;
+        private TempChanged _tempChange;
+
+        public double CurrentTemp
+        {
+            get
+            {
+                return _currentTemp;
+            }
+            set
+            {
+                if (_currentTemp != value)
+                {
+                    var prev = _currentTemp;
+                    _currentTemp = value;
+                    OnTempChanged(prev, value); 
+                }
+            }
+        }
+
+        public void TurnOn()
+        {
+            while (CurrentTemp < 100)
+            {
+                Thread.Sleep(1000);
+                CurrentTemp++;
+            }
+        }
+
+        public void RegisterTempWatcher(TempChanged watcher)
+        {
+            _tempChange += watcher;
+        }
+
+        private void OnTempChanged(double prev, double current)
+        {
+            _tempChange(prev, current); 
+        }
+
+
+
+
+    }
+}
---
+++ DelegatesLambdaAndMore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DelegatesLambdaAndMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void LogToConsole()
+    }
+}
